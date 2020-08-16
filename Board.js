fe677dc01f4f0e9060bbb349e59ae6b694d80ced
JustinBoehnen <justinboehnen@gmail.com>
2020-08-15 23:35:57 -0700

    Fixed board rendering and a really dumb redis mistake

--- Board.js
+++ Board.js
@@ -41,10 +41,13 @@ class Board {
         this.pieceMargin = s.pieceMargin
         this.id = Date.now().toString().slice(4, -1)
 
-        if (s.game === 'chess') this.board = this.generateChessBoard()
+        if (this.game === "chess") this.board = this.generateChessBoard()
+        console.log("passed to Board: " + JSON.stringify(s))
+        console.log(this.board)
     }
 
     generateChessBoard() {
+        console.log("in generate chess board")
         const board = Array(8)
 
         //Setup black side
@@ -81,11 +84,50 @@ class Board {
     }
 
     render() {
-        console.log("in render!")
+        console.log("in render")
         let tileSize = this.boardSize / 8
         let inner = ''
 
-        return `<img src="https://user-images.githubusercontent.com/33167265/90324114-63d36500-df1f-11ea-9d3c-c2795750e861.png" height="200"/>`
+        for (let y = 0; y < 8; ++y) {
+            for (let x = 0; x < 8; ++x) {
+                if (x % 2 === 0) {
+                    if (y % 2 === 0) {
+                        inner += `<rect 
+                            x='${x * tileSize}' 
+                            y='${y * tileSize}' 
+                            width='${tileSize}' 
+                            height='${tileSize}'
+                            style="fill:rgb(255,0,0)"/>`//;stroke-width:3;stroke:rgb(255,0,0)" />`
+                    }
+                    else {
+                        inner += `<rect 
+                        x='${x * tileSize}' 
+                        y='${y * tileSize}' 
+                        width='${tileSize}' 
+                        height='${tileSize}'
+                        style="fill:rgb(0,0,0)"/>`//;stroke-width:3;stroke:rgb(0,0,0)" />`
+                    }
+                }
+                else {
+                    if (y % 2 === 0) {
+                        inner += `<rect 
+                        x='${x * tileSize}' 
+                        y='${y * tileSize}' 
+                        width='${tileSize}' 
+                        height='${tileSize}'
+                        style="fill:rgb(0,0,0)"/>`//;stroke-width:3;stroke:rgb(0,0,0)" />`
+                    }
+                    else {
+                        inner += `<rect 
+                        x='${x * tileSize}' 
+                        y='${y * tileSize}' 
+                        width='${tileSize}' 
+                        height='${tileSize}'
+                        style="fill:rgb(255,0,0)"/>`//;stroke-width:3;stroke:rgb(255,0,0)" />`
+                    }
+                }
+            }
+        }
 
         for (let y = 0; y < 8; ++y) {
             for (let x = 0; x < 8; ++x) {
@@ -112,31 +154,12 @@ class Board {
     }
 
     async save() {
-        await redis.hset([
-            `id:${this.id}`,
-            'game',
-            this.boardSize.toString(),
-            'bsize',
-            this.boardSize.toString(),
-            'pstyle',
-            this.pieceStyle,
-            'pmargin',
-            this.pieceMargin.toString(),
-            'board',
-            JSON.stringify(this.board),
-        ])
+        await redis.hset(`id:${this.id}`, 'game', this.game)
+        await redis.hset(`id:${this.id}`, 'bsize', this.boardSize.toString())
+        await redis.hset(`id:${this.id}`, 'pstyle', this.pieceStyle)
+        await redis.hset(`id:${this.id}`, 'pmargin', this.pieceMargin.toString())
+        await redis.hset(`id:${this.id}`, 'board', JSON.stringify(this.board))
     }
-
-    /*static async getBoardById(id) {
-        const boardSize = await redis.hget(`game:${id}`, 'size')
-
-        if (boardSize === null) throw { message: 'Invalid game id' }
-
-        const board = new Board(boardSize)
-        board.board = JSON.parse(await redis.hget(`game:${id}`, 'board'))
-
-        return board
-    }*/
 
     static async getBoardById(id) {
         const game = await redis.hget(`id:${id}`, 'game')
@@ -145,9 +168,9 @@ class Board {
 
         let s = {
             game: game,
-            boardSize: await redis.hget(`id:${id}`, 'boardSize'),
-            pieceStyle: await redis.hget(`id:${id}`, 'pieceStyle'),
-            pieceMargin: await redis.hget(`id:${id}`, 'pieceMargin'),
+            boardSize: parseInt(await redis.hget(`id:${id}`, 'bsize')),
+            pieceStyle: await redis.hget(`id:${id}`, 'pstyle'),
+            pieceMargin: parseInt(await redis.hget(`id:${id}`, 'pmargin')),
         }
 
         const board = new Board(s)
