8693286e9c0c8d8fb11f0de2a4f0b3d2cd4e1e8c
mengistristen <tristen.mengis@gmail.com>
2020-08-19 23:09:43 -0700

    Added ip address when starting server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = require('./app')
+const { networkInterfaces } = require('os')
 
 const PORT = 8000
-app.listen(PORT, () =>
+app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
-)
+    console.log(`Or at http://${require('ip').address()}:${PORT}`)
+})
