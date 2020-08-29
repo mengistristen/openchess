844b6b6223dac49de1bcec05a3fcb792159c43c2
mengistristen <tristen.mengis@gmail.com>
2020-08-28 21:37:52 -0700

    Added port env variable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
     console.log(`Or at http://${require('ip').address()}:${PORT}`)
